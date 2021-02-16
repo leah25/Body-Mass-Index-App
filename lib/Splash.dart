25dcc95604ad473c66086715a89482971e4da9fc
Sharon Leah <sharonleah25@gmail.com>
2021-02-16 08:51:37 +0300

    updated spash screen navigation

--- lib/Splash.dart
+++ lib/Splash.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bodymassindexapp/InputPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -10,12 +11,18 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    Timer(Duration(seconds: 5), () => Navigator.push(context, MaterialPageRoute(builder:(context)   => InputPage(),),),);
+    Timer(
+      Duration(seconds: 5),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => InputPage(),
+        ),
+      ),
+    );
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -32,27 +39,42 @@ class _SplashScreenState extends State<SplashScreen> {
               Expanded(
                 flex: 2,
                 child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 100.0,
+                        backgroundColor: Color(0xFFFFFFFF),
+                        backgroundImage: AssetImage('images/b1.jpg'),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        'BMI CALCULATOR',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    CircleAvatar(
-                      radius: 100.0,
-                      backgroundColor: Color(0xFFFFFFFF),
-                      backgroundImage: AssetImage('images/b1.jpg'),
-                    ),
-                    SizedBox(height: 10.0,),
-                    Text('BMI CALCULATOR', style: TextStyle(color: Colors.white, fontSize: 30.0,fontWeight:FontWeight.bold),),
+                    CircularProgressIndicator(),
+                    SizedBox(height: 20),
+                    Text(
+                      'Curious to know about your Body Mass Index (BMI)?',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    )
                   ],
                 ),
-              ),),
-              Expanded(child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircularProgressIndicator(),
-                  SizedBox(height: 20),
-                  Text('Curious to know about your Body Mass Index (BMI)?',style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
-                ],
-              ),),
+              ),
             ],
           ),
         ],
